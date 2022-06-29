e7d06ae76e5a9b5879e27da4c4b43d1493f6653b
Sveta <sveta.zheleznova.03@mail.ru>
2022-06-29 17:27:00 +0300

    small changes to start the program

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,15 @@ fn main() -> ! {
     let peripherals = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(peripherals);
 
-//    let mut led = pins.d13.into_output();
-    let mut pin_9 = pins.d9.into_output();
+    let mut led = pins.d13.into_output();
+//    let mut pin_9 = pins.d9.into_output();
 
-    let tc1 = dp.TC1;
-    tc1.icr1.write(|w| unsafe { w.bits(4999) });
-    tc1.tccr1a
-        .write(|w| w.wgm1().bits(0b10).com1a().match_clear());
-    tc1.tccr1b
-        .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
+//    let tc1 = dp.TC1;
+//    tc1.icr1.write(|w| unsafe { w.bits(4999) });
+//    tc1.tccr1a
+//        .write(|w| w.wgm1().bits(0b10).com1a().match_clear());
+//    tc1.tccr1b
+//        .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
     loop{
         led.toggle();
         arduino_hal::delay_ms(1000);
