11863001bf81c3c303435a7284efa8925fd87206
Sveta <sveta.zheleznova.03@mail.ru>
2022-06-30 13:11:32 +0300

    working w battery

--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,33 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    // let mut val = 0;
-
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
     let mut servo_pin = pins.d9.into_output();
-
     let mut led = pins.d13.into_output();
+
+    let mut val;
     loop {
-        // val (angle)= 180;
+        val = 180; // angle
         led.toggle();
-        for i in 0..180{
-            let pulse_width = (i * 11) + 500;
+        for _i in 0..50{
+            let pulse_width = (val * 11) + 500;
             servo_pin.set_high();
             arduino_hal::delay_us(pulse_width);
             servo_pin.set_low();
-            arduino_hal::delay_ms(2);
+            arduino_hal::delay_us(20000 - pulse_width - 1000);
         }
-        // val (angle) = 0;
-        arduino_hal::delay_ms(1000);
-        for i in (0..180).rev(){
-            let pulse_width = (i * 11) + 500;
+        val = 0;
+        arduino_hal::delay_ms(2000);
+        for _i in (0..50).rev(){
+            let pulse_width = (val * 11) + 500;
             servo_pin.set_high();
             arduino_hal::delay_us(pulse_width);
             servo_pin.set_low();
-            arduino_hal::delay_ms(2);
-            // let pulse_width = 200;
-            // servo_pin.set_high();
-            // arduino_hal::delay_us(pulse_width);
-            // servo_pin.set_low();
-            // arduino_hal::delay_us(20 - pulse_width / 1000);
+            arduino_hal::delay_us(20000 - pulse_width - 1000);
         }
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(2000);
 
     }
 }
