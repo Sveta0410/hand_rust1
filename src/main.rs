4ae05e5cb6d8099f01ed688138bc82df88260331
Sveta <sveta.zheleznova.03@mail.ru>
2022-07-05 21:52:44 +0300

    serial not working (does not fit into 'flash' memory)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod servo;
 use servo::ServoMotor;
 
 use core::panic::PanicInfo;
+use avr_hal_generic::ufmt;
+use embedded_hal::serial::Read;
+use arduino_hal::prelude::*;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -15,6 +18,8 @@ fn panic(_info: &PanicInfo) -> ! {
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 9600);
+    ufmt::uwriteln!(&mut serial, "{}", 10).void_unwrap();
 
     let mut servo_pin = pins.d10.into_output();
     let _start = ServoMotor::write_10(&mut servo_pin, 0);
@@ -23,12 +28,11 @@ fn main() -> ! {
     let _start = ServoMotor::write_5(&mut servo_pin1, 0);
 
     let mut led = pins.d13.into_output(); // delete later
-
-    // let mut val = 180; // use later
+    let b:u32 = nb::block!(serial.read()).unwrap().into();
 
     loop {
         led.toggle();
-        let _work = ServoMotor::write_10(&mut servo_pin, 180);
+        let _work = ServoMotor::write_10(&mut servo_pin, b);
         arduino_hal::delay_ms(2000);
         let _work = ServoMotor::write_10(&mut servo_pin, 0);
         arduino_hal::delay_ms(2000);
