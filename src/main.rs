2403a352572f7deaa449fe19bb89ae4041a9a593
Sveta <sveta.zheleznova.03@mail.ru>
2022-07-22 20:34:23 +0300

    for all fingers

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 mod servo;
 use servo::ServoMotor;
 
-use core::panic::PanicInfo;
+// use core::panic::PanicInfo;
 
 use arduino_hal::prelude::*;
 use panic_halt as _;
@@ -13,30 +13,28 @@ use embedded_hal::serial::Read;
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    let mut start = false;
-    let mut counter = 0;
-
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").void_unwrap();
 
-    let mut servo_pin_10 = pins.d10.into_output();
-    let _start = ServoMotor::write_10(&mut servo_pin_10, 0);
-
     let mut servo_pin_5 = pins.d5.into_output();
     let _start = ServoMotor::write_5(&mut servo_pin_5, 0);
 
-    let mut led = pins.d13.into_output(); // delete later
+    let mut servo_pin_6 = pins.d6.into_output();
+    let _start = ServoMotor::write_6(&mut servo_pin_6, 0);
 
-    let mut b1 = 0;
-    let mut b2 = 0;
-    let mut b3 = 0;
-    let mut b4 = 0;
-    let mut b5 = 0;
+    let mut servo_pin_9 = pins.d9.into_output();
+    let _start = ServoMotor::write_9(&mut servo_pin_9, 0);
+
+    let mut servo_pin_10 = pins.d10.into_output();
+    let _start = ServoMotor::write_10(&mut servo_pin_10, 0);
+
+    let mut servo_pin_11 = pins.d11.into_output();
+    let _start = ServoMotor::write_11(&mut servo_pin_11, 0);
+
     loop {
-
         let b = nb::block!(serial.read()).void_unwrap();
         if b == 36 {
             let b1 = nb::block!(serial.read()).void_unwrap();
@@ -44,33 +42,35 @@ fn main() -> ! {
             let b3 = nb::block!(serial.read()).void_unwrap();
             let b4 = nb::block!(serial.read()).void_unwrap();
             let b5 = nb::block!(serial.read()).void_unwrap();
-            start = false;
 
             if b3 > 36 {
                 ufmt::uwriteln!(&mut serial, "Got {} {} {} {} {}!\r", b1, b2, b3, b4, b5).void_unwrap();
                 if b1 == 48 {
-                    let _work = ServoMotor::write_10(&mut servo_pin_10, 0);
-                } else if b1 == 49 {
-                    let _work = ServoMotor::write_10(&mut servo_pin_10, 180);
-                }
-                if b2 == 48 {
                     let _work = ServoMotor::write_5(&mut servo_pin_5, 0);
-                } else if b2 == 49 {
+                } else if b1 == 49 {
                     let _work = ServoMotor::write_5(&mut servo_pin_5, 180);
                 }
-                // if b3 == 48{
-                //     let _work = ServoMotor::write_10(&mut servo_pin, 0);
-                // } else if b3 == 49{
-                //     let _work = ServoMotor::write_10(&mut servo_pin, 180);
-                // if b4 == 48{
-                //     let _work = ServoMotor::write_10(&mut servo_pin, 0);
-                // } else if b4 == 49{
-                //     let _work = ServoMotor::write_10(&mut servo_pin, 180);
-                // if b5 == 48{
-                //     let _work = ServoMotor::write_10(&mut servo_pin, 0);
-                // } else if b5 == 49{
-                //     let _work = ServoMotor::write_10(&mut servo_pin, 180);}
-            }
+                if b2 == 48 {
+                    let _work = ServoMotor::write_6(&mut servo_pin_6, 0);
+                } else if b2 == 49 {
+                    let _work = ServoMotor::write_6(&mut servo_pin_6, 180);
+                }
+                if b3 == 48 {
+                    let _work = ServoMotor::write_9(&mut servo_pin_9, 0);
+                } else if b3 == 49 {
+                    let _work = ServoMotor::write_9(&mut servo_pin_9, 180);
+                }
+                if b4 == 48 {
+                    let _work = ServoMotor::write_10(&mut servo_pin_10, 0);
+                } else if b4 == 49 {
+                    let _work = ServoMotor::write_10(&mut servo_pin_10, 180);
+                }
+                if b5 == 48 {
+                    let _work = ServoMotor::write_11(&mut servo_pin_11, 0);
+                } else if b5 == 49 {
+                    let _work = ServoMotor::write_11(&mut servo_pin_11, 180);
+                }
             }
         }
     }
+}
--- src/servo.rs
+++ src/servo.rs
@@ -15,7 +15,39 @@ impl ServoMotor{
         }
 
     }
+    pub fn write_6(my_pin:  &mut avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Output, atmega_hal::port::PD6>, angle: u32)
+        {
+        for _i in 0..50{
+            let pulse_width = (angle * 11) + 500;
+            my_pin.set_high();
+            arduino_hal::delay_us(pulse_width);
+            my_pin.set_low();
+            arduino_hal::delay_us(20000 - pulse_width);
+        }
+
+    }
+    pub fn write_9(my_pin:  &mut avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Output, atmega_hal::port::PB1>, angle: u32)
+        {
+        for _i in 0..50{
+            let pulse_width = (angle * 11) + 500;
+            my_pin.set_high();
+            arduino_hal::delay_us(pulse_width);
+            my_pin.set_low();
+            arduino_hal::delay_us(20000 - pulse_width);
+        }
+
+    }
     pub fn write_10(my_pin:  &mut avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Output, atmega_hal::port::PB2>, angle: u32)
+        {
+        for _i in 0..50{
+            let pulse_width = (angle * 11) + 500;
+            my_pin.set_high();
+            arduino_hal::delay_us(pulse_width);
+            my_pin.set_low();
+            arduino_hal::delay_us(20000 - pulse_width);
+        }
+    }
+    pub fn write_11(my_pin:  &mut avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Output, atmega_hal::port::PB3>, angle: u32)
         {
         for _i in 0..50{
             let pulse_width = (angle * 11) + 500;
